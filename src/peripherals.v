68baabf2bbf8db645f8b6a474a2823cd4f18458f
Matt Venn <matt@mattvenn.net>
2025-07-28 17:47:50 +0200

    add Matt PWM

--- src/peripherals.v
+++ src/peripherals.v
@@ -496,7 +496,7 @@ module tinyQV_peripherals (
         .data_out(data_from_simple_peri[4])
     );
 
-    tqvp_byte_example i_user_simple05 (
+    tqvp_matt_pwm matt_pwm (
         .clk(clk),
         .rst_n(rst_n),
 
