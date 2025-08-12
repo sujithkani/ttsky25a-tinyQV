3d0f49f0517b9d06f10b51467ff1c8a80ef2a6f9
RebeccaRGB <beckiejo@gmail.com>
2025-08-12 14:37:25 -0700

    Hardware UTF Encoder/Decoder - https://github.com/RebeccaRGB/tinyqv-hardware-utf8

--- src/peripherals.v
+++ src/peripherals.v
@@ -542,7 +542,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .data_out(data_from_simple_peri[7])
     );
 
-    tqvp_byte_example i_user_simple08 (
+    tqvp_rebeccargb_hardware_utf8 hardware_utf8 (
         .clk(clk),
         .rst_n(rst_n),
 
