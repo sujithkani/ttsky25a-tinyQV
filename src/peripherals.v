1444d2b7e6e4e4ab46ab09e00b50ebc4e6dee9bd
Alessandro Vargiu <osirisvbb@gmail.com>
2025-08-14 11:39:16 +0200

    Correction on peripheral instance, crc32 is on ID 25

--- src/peripherals.v
+++ src/peripherals.v
@@ -526,7 +526,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .data_out(data_from_simple_peri[6])
     );
 
-    tqvp_crc32  crc32(
+    tqvp_byte_example i_user_simple07 (
         .clk(clk),
         .rst_n(rst_n),
 
@@ -556,7 +556,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .data_out(data_from_simple_peri[8])
     );
 
-    tqvp_byte_example i_user_simple09 (
+    tqvp_crc32 crc32(
         .clk(clk),
         .rst_n(rst_n),
 
